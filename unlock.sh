20bf70f40896c5f545a0fcfd135e1cf45d9fe1b0
urnuts <51977940+urnuts@users.noreply.github.com>
2021-04-21 22:45:43 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -26,7 +26,7 @@ if [ $? -eq 0 ]; then
     echo "server=8.8.8.8" >> /etc/dnsmasq.d/unlock.conf
     echo "server=8.8.4.4" >> /etc/dnsmasq.d/unlock.conf
     echo "server=9.9.9.9" >> /etc/dnsmasq.d/unlock.conf
-    echo "#NetFlix" >> /etc/dnsmasq.d/unlock.conf
+    echo "# 奈飞NetFlix" >> /etc/dnsmasq.d/unlock.conf
     echo "server=/fast.com/$1" >> /etc/dnsmasq.d/unlock.conf
     echo "server=/netflix.com/$1" >> /etc/dnsmasq.d/unlock.conf
     echo "server=/netflix.net/$1" >> /etc/dnsmasq.d/unlock.conf
